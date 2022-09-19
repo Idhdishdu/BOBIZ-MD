5164d4455678e765ec7d45aa916f53ddf849691e
Idhdishdu <113697568+Idhdishdu@users.noreply.github.com>
2022-09-19 19:23:38 +0200

    Update config.js

--- config.js
+++ config.js
@@ -11,20 +11,20 @@ DATABASE_URL = process.env.DATABASE_URL === undefined ? './tmp' : process.env.DA
 DEBUG = process.env.DEBUG === undefined ? false : convertToBool(process.env.DEBUG);
 
 module.exports = {
-    SESSION: process.env.BOBIZ_SESSION === undefined ? '' : process.env.BOBIZ_SESSION,
+    SESSION: process.env.BOBIZ_SESSION === undefined ? '' : process.env.ELGAZAR_SESSION,
     URL_1NAME: process.env.URL_1NAME === undefined ? '🎃شات المطور🎃' : process.env.URL_1NAME,
-    URL_1LINK: process.env.URL_1LINK === undefined ? 'https://wa.me/+967770500831 ' : process.env.URL_1LINK,
-    URL_2NAME: process.env.URL_2NAME === undefined ? '✨قروبي✨' : process.env.URL_2NAME,
+    URL_1LINK: process.env.URL_1LINK === undefined ? 'https://wa.me/+201098906252 ' : process.env.URL_1LINK,
+    URL_2NAME: process.env.URL_2NAME === undefined ? '✨جروبي✨' : process.env.URL_2NAME,
     URL_2LINK: process.env.URL_2LINK === undefined ? 'https://chat.whatsapp.com/Gb62eN2S5yI9mhfdn6Ma4z' : process.env.URL_2LINK,
     FOOTER: process.env.FOOTER === undefined ? 'RAIZELBOT-MD  https://wa.me/+967770500831  تابعني على الواتساب ' : process.env.FOOTER,
-    CAPTION: process.env.CAPTION === undefined ? '🔱 𝚁𝚊𝚒𝚣𝚎𝚕 𝙶𝚎𝚘𝚛𝚐𝚎 🔱 𝙱𝙾𝚃 𝙼𝙳 https://wa.me/+967770500831 😀' : process.env.CAPTION,
+    CAPTION: process.env.CAPTION === undefined ? '🔱 ᗴᒪᘜᗩᘔᗩᖇ ᗩᒪᗯᗩᘔᗴᖇ 🔱 𝙱𝙾𝚃 𝙼𝙳 https://wa.me/+201098906252 😀' : process.env.CAPTION,
     ALIVEMSG: process.env.ALIVE_MESSAGE === undefined ? 'default' : process.env.ALIVE_MESSAGE,
-    ALIVE_LOGO: process.env.ALIVE_LOGO === undefined ? 'https://telegra.ph/file/201b5c02be1e336a29c60.jpg' : process.env.ALIVE_LOGO,
+    ALIVE_LOGO: process.env.ALIVE_LOGO === undefined ? 'https://telegra.ph/file/9d09a00e6c16ca29eead1.jpg' : process.env.ALIVE_LOGO,
     SONG_DOWN: process.env.SONG_DOWN === undefined ? '*📥Downloading your song...*' : process.env.SONG_DOWN,
     SONG_UP: process.env.SONG_UP === undefined ? '*📤Uploading your song...*' : process.env.SONG_UP,
     VIDEO_DOWN: process.env.VIDEO_DOWN === undefined ? '*📥Downloading your video...*' : process.env.VIDEO_DOWN,
     VIDEO_UP: process.env.VIDEO_UP === undefined ? '*📤Uploading your video...*' : process.env.VIDEO_UP,
     FILE_DOWN: process.env.FILE_DOWN === undefined ? '*📥Downloading your file...*' : process.env.FILE_DOWN,
     FILE_UP: process.env.FILE_UP === undefined ? '*📤Uploading your file...*' : process.env.FILE_UP,
-    STIC_WM:process.env.STIC_WM === undefined ? '🔱 𝚁𝚊𝚒𝚣𝚎𝚕 𝙶𝚎𝚘𝚛𝚐𝚎 🔱' : process.env.STIC_WM,
+    STIC_WM:process.env.STIC_WM === undefined ? '🔱 ᗴᒪᘜᗩᘔᗩᖇ ᗩᒪᗯᗩᘔᗴᖇ 🔱' : process.env.STIC_WM,
 };
